7b815b6fb18745ac3a243a207970a7f1b03c27da
harrisbang2 <harrisbang2@gmail.com>
2023-11-19 15:43:11 +0900

    retestin

---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "header.css"
+function Header(){
+return (
+    <div className="header">
+        <div className="header_Logo">
+        <h1>Header</h1>
+        
+        </div>
+    </div>
+);
+}
+export default Header;
