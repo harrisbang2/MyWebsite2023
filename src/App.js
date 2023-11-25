cc2d6e3cf8897a56d4787c0018967f4d7e0e95e9
harrisbang2 <harrisbang2@gmail.com>
2023-11-25 21:40:27 +0900

    home img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './header';
+import Home from './home.js';
 function App() {
   return (
     <div className="App">
     <Header />
-    {/*home*/}
+    <Home />
     </div>
   );
 }
