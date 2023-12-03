a15fb071fbb95b406e9b8eb799f550140be37b8c
harrisbang2 <harrisbang2@gmail.com>
2023-12-03 22:52:05 +0900

    ads

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import "./checkout.css"
+import Subtotal from"./Subtotal.js"
 function Checkout() {
   return (
     <div className='checkout'>
       <div className="checkout_left">
       <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout_ad" /> 
         <h2 className='checkout_title'>Your Shopping Basket</h2>
+      {/*cccc*/}
+       {/*cccc*/}
+      {/*cccc*/}
       </div>
       <div className="checkout_right">
-        <h2>check_out right</h2>
-        {/*cccc*/}
-        {/*cccc*/}
-        {/*cccc*/}
+        
+        <Subtotal />
       </div>
     </div>
   )
